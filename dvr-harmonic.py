5b4b404d442db3203d589fcc675705392bec2647
Masaaki Saitow <msaitow514@gmail.com>
2022-05-13 01:16:04 +0900

    update

--- dvr-harmonic.py
+++ dvr-harmonic.py
@@ -1,5 +1,5 @@
-from numpy import linalg as la
 import numpy as np
+import time as tm
 from functools import reduce
 
 # -:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|
@@ -9,84 +9,98 @@ from functools import reduce
 # -:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|-:|
 
 # 区間数
-dim    = 2000
+dim    :int   = 7000
 # 積分範囲
-xinit  = -20.0
-xfinal =  20.0
+xinit  :float = -50.0
+xfinal :float =  50.0
 # 一区間の長さ
-dx     = (xfinal-xinit)/dim
+dx :float = (xfinal-xinit)/dim
 
 # プランク定数
-hbar = 1.0
+hbar :float = 1.0
 # 質量
-m    = 1.0
+m : float   = 1.0
 
 # 運動エネルギー行列要素の計算
-def formKinetic():
-    k = np.zeros([dim,dim])
+def formKinetic() -> np.ndarray:
+    k :np.ndarray = np.zeros([dim,dim])
     for i in range(dim):
         for j in range(i,dim):
-            fac = hbar**2 / (2*m) * (-1.0)**(i-j) / dx**2.0 
+            fac :float = hbar**2 / (2*m) * (-1.0)**(i-j) / dx**2.0 
             if i==j: k[i,j] = fac * np.pi**2 / 3.0
             else:    k[i,j] = k[j,i] = fac * 2 / (i-j)**2
     return k
 
 # 調和振動子ポテンシャルエネルギー行列要素の計算
-def formPotential():
+def formPotential() -> np.ndarray:
     # 振動子の力定数
-    k     = 10.0
+    k :float = 10.0
     # 振動数
-    omega = np.sqrt(k/m)
-    v = np.zeros([dim,dim])
+    omega :np.ndarray = np.sqrt(k/m)
+    v :np.ndarray = np.zeros([dim,dim])
     for i in range(dim):
-        x = xinit + i*dx
+        x :float = xinit + i*dx
         v[i,i] = 0.5*k*x**2
     return v
 
+# DVR波動関数（psi）を新規ファイル（ファイル名:f_name）にcsv形式でnumstates状態だけ保存
+# 生成されたcsvファイルはExcelなどで開くことができ、波動関数をプロットできる
+def saveWFs(psi :np.ndarray, f_name :str, numstates :int) -> None:
+    outfile = open(f_name, "w")
+    #
+    label :list[str] = ["x"]
+    for i in range(numstates): label.append("state{0:d}".format(i))
+    l_str :str = reduce(lambda x,y: x + ', ' + y, label)
+    outfile.write(l_str+'\n')
+    for p in range(dim):
+        data :list[float] = [xinit + p*dx]
+        for i in range(numstates): data.append(psi[p,i])
+        d_str :str = reduce(lambda x,y: x + ', ' + y, map(lambda x: "{0:.5e}".format(x), data))
+        outfile.write(d_str+'\n')
+    outfile.close()
+    
 # 厳密な調和振動子のエネルギー（比較用）
-def calcEnergies():
+def calcEnergies() -> np.ndarray:
     # 振動子の力定数    
-    k     = 10.0
+    k :float = 10.0
     # 振動数
-    omega = np.sqrt(k/m)
-    e = np.zeros([dim])
+    omega :float = np.sqrt(k/m)
+    e :np.ndarray = np.zeros([dim])
     for i in range(dim): e[i] = hbar*omega*(i+0.5)
     return e
 
+# 厳密なエネルギーとDVR計算による近似値を比較し、何状態までであれば1e-10以下の桁数で厳密な値と一致するかを比較
+def compareEnergies(e1: np.ndarray, e2: np.ndarray) -> int:
+    error = e1-e2
+    idx = -1
+    for i in range(error.shape[0]):
+        if abs(error[i]) >= 1.0e-10:
+            idx = i
+            break
+    return idx
+
+# プログラム本体
 if __name__ == '__main__':
+    tinit = tm.perf_counter()
     # 各行列要素を構築
-    t = formKinetic()
-    v = formPotential()
+    t :np.ndarray = formKinetic()
+    v :np.ndarray = formPotential()
     # ハミルトニアン行列
-    h = t + v
+    h :np.ndarray = t + v
     # ハミルトニアン行列を対角化
-    edvr, psi = la.eigh(h)
+    edvr, psi = np.linalg.eigh(h)
     # 厳密なエネルギーも計算し、これらを比較
-    eexact = calcEnergies()
+    eexact :np.ndarray = calcEnergies()
     print("Energies (DVR)")
     print(edvr)
     print("Energies (Exact)")    
     print(eexact)
-
+    print("\n==|> DVR energies match up to {0:d} states with the exact ones!".format(compareEnergies(edvr,eexact)) )
     # 50状態の波動関数をでcsvファイル（wf.csv）に保存
-    numstates = 50
-    outfile   = open("wf.csv", "w")
-
-    label = ["x"]
-    for i in range(numstates): label.append("state{0:d}".format(i))
-    l_str = reduce(lambda x,y: x + ', ' + y, label)
-    outfile.write(l_str+'\n')
-    for p in range(dim):
-        data = [xinit + p*dx]
-        for i in range(numstates): data.append(psi[p,i])
-        #d_str = []
-        #for d in data: d_str.append("{0:.5e}".format(d))
-        d_str = reduce(lambda x,y: x + ', ' + y, map(lambda x: "{0:.5e}".format(x), data))
-        outfile.write(d_str+'\n')
-        #print(d_str)
-
-    outfile.close()
+    saveWFs(psi,"wf.csv",50)
+    tfinal = tm.perf_counter()
+    # プログラムの実行時間を表示
+    print("Elapsed time: {0:.2f} sec.".format(tfinal-tinit))
     
-        
     
     
